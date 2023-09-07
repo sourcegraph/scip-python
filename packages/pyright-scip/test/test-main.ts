b8df30d490e198aa682634edafa1bb2f5ae8db47
Varun Gandhi <varun.gandhi@sourcegraph.com>
2023-09-07 14:12:00 +0800

    fix: Reset cwd in test suite

--- packages/pyright-scip/test/test-main.ts
+++ packages/pyright-scip/test/test-main.ts
@@ -4,13 +4,14 @@ import * as fs from 'fs';
 
 function testMain(mode: 'check' | 'update'): void {
     const nodePath = process.argv[0];
+    const startCwd = process.cwd();
     // Returns list of subdir names, not absolute paths.
     const inputDir = path.join('.', 'snapshots', 'input');
     const subdirNames = fs.readdirSync(inputDir);
     const packageInfoPath = path.join('.', 'snapshots', 'packageInfo.json');
     const packageInfo = JSON.parse(fs.readFileSync(packageInfoPath, 'utf8'));
     for (const subdirName of subdirNames) {
-        console.assert(path.dirname(subdirName) === '.');
+        console.assert(!subdirName.includes(path.sep));
         let projectName = packageInfo['default']['name'];
         let projectVersion = packageInfo['default']['version'];
         if (subdirName in packageInfo['special']) {
@@ -36,6 +37,8 @@ function testMain(mode: 'check' | 'update'): void {
             argv.push('--check');
         }
         main(argv);
+        // main changes the working directory; reset it.
+        process.chdir(startCwd);
     }
 }
 
