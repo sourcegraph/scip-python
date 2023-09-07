ad7806fab8f301193eaceb3cd45083dd0a3eaa5f
Varun Gandhi <varun.gandhi@sourcegraph.com>
2023-09-07 13:38:41 +0800

    test: Read project-info.json for name and version

--- packages/pyright-scip/test/test-main.ts
+++ packages/pyright-scip/test/test-main.ts
@@ -5,9 +5,18 @@ import * as fs from 'fs';
 function testMain(mode: 'check' | 'update'): void {
     const nodePath = process.argv[0];
     // Returns list of subdir names, not absolute paths.
-    const subdirNames = fs.readdirSync('./snapshots/input');
+    const inputDir = path.join('.', 'snapshots', 'input');
+    const subdirNames = fs.readdirSync(inputDir);
     for (const subdirName of subdirNames) {
-        console.assert(subdirName.includes('/') === false);
+        console.assert(path.dirname(subdirName) === '');
+        const projectInfoPath = path.join(inputDir, subdirName, 'project-info.json');
+        let projectName = 'snapshot-util';
+        let projectVersion = '0.1';
+        if (fs.existsSync(projectInfoPath)) {
+            const projectInfo = JSON.parse(fs.readFileSync(projectInfoPath).toString());
+            projectName = projectInfo['name'];
+            projectVersion = projectInfo['version'];
+        }
         const argv = [
             nodePath,
             path.resolve('./index.js'),
@@ -17,9 +26,9 @@ function testMain(mode: 'check' | 'update'): void {
             'snapshots/testEnv.json',
             '--quiet',
             '--project-name',
-            'snapshot-util',
+            projectName,
             '--project-version',
-            '0.1',
+            projectVersion,
             '--only',
             subdirName,
         ];
