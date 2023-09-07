2f0510700e8c6f023d7b0c0016f4311e8689275e
Varun Gandhi <varun.gandhi@sourcegraph.com>
2023-09-07 13:30:25 +0800

    test: Customize package name and version in test

--- packages/pyright-scip/test/test-main.ts
+++ packages/pyright-scip/test/test-main.ts
@@ -1,25 +1,33 @@
 import { main } from '../src/main-impl';
 import * as path from 'path';
+import * as fs from 'fs';
 
 function testMain(mode: 'check' | 'update'): void {
     const nodePath = process.argv[0];
-    const argv = [
-        nodePath,
-        path.resolve('./index.js'),
-        'snapshot-dir',
-        './snapshots',
-        '--environment',
-        'snapshots/testEnv.json',
-        '--quiet',
-        '--project-name',
-        'snapshot-util',
-        '--project-version',
-        '0.1',
-    ];
-    if (mode === 'check') {
-        argv.push('--check');
+    // Returns list of subdir names, not absolute paths.
+    const subdirNames = fs.readdirSync('./snapshots/input');
+    for (const subdirName of subdirNames) {
+        console.assert(subdirName.includes('/') === false);
+        const argv = [
+            nodePath,
+            path.resolve('./index.js'),
+            'snapshot-dir',
+            './snapshots',
+            '--environment',
+            'snapshots/testEnv.json',
+            '--quiet',
+            '--project-name',
+            'snapshot-util',
+            '--project-version',
+            '0.1',
+            '--only',
+            subdirName,
+        ];
+        if (mode === 'check') {
+            argv.push('--check');
+        }
+        main(argv);
     }
-    main(argv);
 }
 
 if (process.argv.indexOf('--check') !== -1) {
