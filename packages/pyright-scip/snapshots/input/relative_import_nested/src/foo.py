514bf16a719016ef82a06981242715340bc755d1
Titouan Launay <titouan.launay@lleedpartners.com>
2023-11-08 16:30:03 +0100

    #91 - adds nested resolution edge case

---
+++ packages/pyright-scip/snapshots/input/relative_import_nested/src/foo.py
@@ -0,0 +1,13 @@
+def exported_function():
+    return "function"
+
+class MyClass:
+    """This is a class and it is cool"""
+
+    def __init__(self):
+        pass
+
+    def exported_function(self):
+        return "exported"
+
+this_class = MyClass()
