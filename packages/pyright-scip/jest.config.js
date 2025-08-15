36a0722b44e4df9c98115489d0895a29b6272eb9
jupblb <michal.kielbowicz@sourcegraph.com>
2025-08-15 15:23:21 +0200

    style: Format Jest config files with Prettier

--- packages/pyright-scip/jest.config.js
+++ packages/pyright-scip/jest.config.js
@@ -20,10 +20,13 @@ module.exports = {
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
     moduleNameMapper: {
         ...pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>' }),
-        '^typescript-char$': '<rootDir>/../pyright-internal/node_modules/.pnpm/typescript-char@0.0.0/node_modules/typescript-char',
+        '^typescript-char$':
+            '<rootDir>/../pyright-internal/node_modules/.pnpm/typescript-char@0.0.0/node_modules/typescript-char',
         '^vscode-uri$': '<rootDir>/../pyright-internal/node_modules/.pnpm/vscode-uri@3.1.0/node_modules/vscode-uri',
-        '^vscode-languageserver-protocol$': '<rootDir>/../pyright-internal/node_modules/.pnpm/vscode-languageserver-protocol@3.17.3/node_modules/vscode-languageserver-protocol',
-        '^vscode-languageserver-types$': '<rootDir>/../pyright-internal/node_modules/.pnpm/vscode-languageserver-types@3.17.3/node_modules/vscode-languageserver-types',
+        '^vscode-languageserver-protocol$':
+            '<rootDir>/../pyright-internal/node_modules/.pnpm/vscode-languageserver-protocol@3.17.3/node_modules/vscode-languageserver-protocol',
+        '^vscode-languageserver-types$':
+            '<rootDir>/../pyright-internal/node_modules/.pnpm/vscode-languageserver-types@3.17.3/node_modules/vscode-languageserver-types',
     },
     globals: {
         'ts-jest': {
